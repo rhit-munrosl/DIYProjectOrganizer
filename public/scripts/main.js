93181798617015acce64e85dbb548c5af8c23ec0
rhit-munrosl <munrosl@rose-hulman.edu>
2020-11-05 03:03:25 -0500

    CRUD on parts

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -27,7 +27,38 @@ rhit.ClassName = class {
 	}
 }
 
+rhit.FB_COLLECTION_PART = "Components";
+rhit.FB_KEY_PART_NAME = "name";
+rhit.FB_KEY_PART_DESC = "description";
+rhit.FB_KEY_PART_LINK = "link";
+rhit.FB_KEY_PART_CATEGORY = "category";
+rhit.FB_KEY_PART_PRICE = "price";
+
+rhit.FB_COLLECTION_CATEGORY = "Category";
+rhit.FB_KEY_CAT_NAME = "name";
+
+rhit.FB_COLLECTION_PROJECT = "Projects";
+rhit.FB_KEY_PROJECT_NAME = "name";
+rhit.FB_KEY_PROJECT_DESC = "description";
+rhit.FB_KEY_PROJECT_RESOURCES = "resources";
+rhit.FB_KEY_PROJECT_PARTS = "components";
+
+rhit.FB_COLLECTION_RESOURCE = "Resource";
+rhit.FB_KEY_RESOURCE_CONTENT = "content";
+rhit.FB_KEY_RESOURCE_NAME = "name";
+rhit.FB_KEY_RESOURCE_DESC = "description";
+
+rhit.FB_KEY_AUTHOR = "author";
+
 rhit.fbAuthManager = null;
+rhit.partsManager = null;
+
+function htmlToElement(html) {
+	var template = document.createElement('template');
+	html = html.trim();
+	template.innerHTML = html;
+	return template.content.firstChild;
+}
 
 rhit.FbAuthManager = class {
 	constructor() {
@@ -65,6 +96,192 @@ rhit.FbAuthManager = class {
 		return this._user.uid;
 	}
 
+}
+
+rhit.PartsLibraryManager = class {
+	constructor(userid) {
+		this._uid = userid;
+		this._searchQuery = "";
+		this._documentSnapshots = [];
+		this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_PART);
+		this._unsubscribe = null;
+	}
+
+	addPart(n, d, p, l, c) {
+		this._ref.add({
+			[rhit.FB_KEY_PART_NAME]: n,
+			[rhit.FB_KEY_PART_DESC]: d,
+			[rhit.FB_KEY_PART_LINK]: l,
+			[rhit.FB_KEY_PART_PRICE]: p,
+			[rhit.FB_KEY_PART_CATEGORY]: c,
+			[rhit.FB_KEY_AUTHOR]: this._uid,
+		}).catch(function (error) {
+			console.log(error);
+		});
+	}
+
+	updatePart(id, n, d, p, l, c) {
+		this._ref.doc(id).update({
+			[rhit.FB_KEY_PART_NAME]: n,
+			[rhit.FB_KEY_PART_DESC]: d,
+			[rhit.FB_KEY_PART_LINK]: l,
+			[rhit.FB_KEY_PART_PRICE]: p,
+			[rhit.FB_KEY_PART_CATEGORY]: c,
+		}).catch((err)=>{
+			console.log("error");
+		});
+	}
+
+	deletePart(id) {
+		return this._ref.doc(id).delete();
+	}
+
+	beginListening(changeListener) {
+		let query = this._ref.orderBy(rhit.FB_KEY_PART_NAME, "desc").limit(20).where(rhit.FB_KEY_AUTHOR, "==", this._uid);
+		this._unsubscribe = query.onSnapshot((querySnapshot) => {
+			this._documentSnapshots = querySnapshot.docs;
+			if (changeListener)
+				changeListener();
+		});
+	}
+
+	stopListenting() {
+		this._unsubscribe;
+	}
+
+	getPartAtIndex(index) {
+		const docSnap = this._documentSnapshots[index];
+		const part = new rhit.Part(
+			docSnap.id,
+			docSnap.get(rhit.FB_KEY_PART_NAME),
+			docSnap.get(rhit.FB_KEY_PART_DESC),
+			docSnap.get(rhit.FB_KEY_PART_PRICE),
+			docSnap.get(rhit.FB_KEY_PART_LINK),
+			docSnap.get(rhit.FB_KEY_PART_CATEGORY),
+		);
+
+		return part;
+	}
+
+	get length() {
+		return this._documentSnapshots.length;
+	}
+
+}
+
+
+
+rhit.PartsLibraryController = class {
+
+	constructor() {
+
+		this.focusedPart = null;
+
+		$('#addCatList li').on('click', function () {
+			document.querySelector('#categoryDropdownAddPart').innerHTML = $(this).text();
+		});
+
+		$('#editCatList li').on('click', function () {
+			document.querySelector('#categoryDropdownEditPart').innerHTML = $(this).text();
+		});
+		//User buttons
+		document.querySelector("#submitAddPart").onclick = (event) => {
+			let name = document.querySelector("#addPartName").value;
+			let desc = document.querySelector("#addPartDesc").value;
+			let price = document.querySelector("#addPartPrice").value;
+			let link = document.querySelector("#addPartLink").value;
+			let cat = document.querySelector("#categoryDropdownAddPart").innerHTML;
+			rhit.partsManager.addPart(name, desc, price, link, cat);
+		}
+
+		document.querySelector("#submitDeletePart").onclick = (event) => {
+			this.deletePart();
+		}
+
+		document.querySelector("#submitEditPart").onclick = (event) => {
+			this.updatePart();
+		}
+
+		rhit.partsManager.beginListening(this.updateGrid.bind(this));
+	}
+
+	_createPart(part) {
+		return htmlToElement(`<div class="part">
+		<h4 class="p-name">${part.name}</h4>
+		<h6 class="p-desc">${part.desc}</h6>
+		<hr>
+		<h6 class="p-desc">$${part.price}</h6>
+	  </div>`);
+	}
+
+	updateGrid() {
+		const container = document.querySelector("#partInfo");
+		const newList = htmlToElement('<div id="partsList" class="grid-container grid-child"></div>');
+		for (let i = 0; i < rhit.partsManager.length; i++) {
+			const part = rhit.partsManager.getPartAtIndex(i);
+			const newCard = this._createPart(part);
+			newCard.onclick = (event) => {
+				this.updateFocus(part);
+			};
+			newList.appendChild(newCard);
+		}
+
+		const oldList = document.querySelector("#partsList");
+		container.appendChild(oldList);
+		oldList.removeAttribute("id");
+		oldList.hidden = true;
+
+		oldList.parentElement.appendChild(newList);
+		oldList.parentElement.appendChild(document.querySelector("#partContainer"));
+
+		if (rhit.partsManager.length > 0){
+			this.updateFocus(rhit.partsManager.getPartAtIndex(0));
+			document.querySelector("#partContainer").style.display = "block";
+		} else {
+			document.querySelector("#partContainer").style.display = "none";
+		}
+	}
+
+	updateFocus(part) {
+		this.focusedPart = part;
+		document.querySelector("#partName").innerHTML = part.name;
+		document.querySelector("#partDesc").innerHTML = part.desc;
+		document.querySelector("#partNav").text = part.link;
+		document.querySelector("#partNav").href = part.link;
+		document.querySelector("#partPrice").innerHTML = "$"+part.price;
+		document.querySelector("#partCat").innerHTML = part.cat;
+
+		document.querySelector("#inputPartName").value = part.name;
+		document.querySelector("#inputPartDesc").value = part.desc;
+		document.querySelector("#inputPartPrice").value = part.price;
+		document.querySelector("#inputPartLink").value = part.link;
+		document.querySelector("#categoryDropdownEditPart").innerHTML = part.cat;
+
+	}
+
+	deletePart() {
+		rhit.partsManager.deletePart(this.focusedPart.id);
+	}
+
+	updatePart() {
+		let newName = document.querySelector("#inputPartName").value; 
+		let newDesc = document.querySelector("#inputPartDesc").value; 
+		let newLink = document.querySelector("#inputPartPrice").value; 
+		let newPrice = document.querySelector("#inputPartLink").value; 
+		let newCat = document.querySelector("#categoryDropdownEditPart").innerHTML; 
+		rhit.partsManager.updatePart(this.focusedPart.id, newName, newDesc, newLink ,newPrice, newCat);
+	}
+}
+
+rhit.Part = class {
+	constructor(id, name, desc, price, link, cat) {
+		this.id = id;
+		this.name = name;
+		this.desc = desc;
+		this.price = price;
+		this.link = link;
+		this.cat = cat;
+	}
 }
 
 rhit.CreateAccountController = class {
@@ -120,6 +337,8 @@ function closeNav() {
 }
 
 rhit.initPage = () => {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString)
 	if (document.querySelector("#mySidenav")) {
 		document.querySelector("#signOutBtn").onclick = (event) => {
 			rhit.fbAuthManager.signOut();
@@ -131,7 +350,17 @@ rhit.initPage = () => {
 	if (document.querySelector("#mainPage")) {
 		new rhit.IndexPageController();
 	}
+	if (document.querySelector("#partsPage")) {
+		rhit.partsManager = new rhit.PartsLibraryManager(rhit.fbAuthManager.uid);
+		new rhit.PartsLibraryController();
+	}
 }
+
+rhit.checkForRedirects = () => {
+	if (!document.querySelector("#createAccPage") && !document.querySelector("#loginPage") && !rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/";
+	}
+};
 
 /* Main */
 /** function and class syntax examples */
@@ -142,6 +371,7 @@ rhit.main = function () {
 	rhit.fbAuthManager.beingListening(() => {
 		console.log("Check for redirects and init the page.");
 		console.log("isSignedIn = ", rhit.fbAuthManager.isSignedIn);
+		rhit.checkForRedirects();
 		rhit.initPage();
 	});
 
